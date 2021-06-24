10b8de8dfb4a9eb0abf9ef7a8b565a43469729df
Wálysson Silva <walysson_silva@hotmail.com>
2021-06-24 18:21:18 -0300

    Utilizando a biblioteca scrollrevealjs

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function() {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
@@ -11,17 +11,17 @@ for (const element of toggle) {
 // Quando clicar em um item do menu, esconder o menu
 const links = document.querySelectorAll('nav ul li a')
 
-for(const link of links) {
-  link.addEventListener('click', function() {
+for (const link of links) {
+  link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
 
 // mudar header da página quando der scroll
-const header = document.querySelector("#header")
+const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -39,4 +39,22 @@ const swiper = new Swiper('.swiper-container', {
   },
   mousewheel: true,
   keyboard: true
-});
+})
+
+// ScrollReveal: Mostrar elementos quando der scroll na página
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: truetext
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
